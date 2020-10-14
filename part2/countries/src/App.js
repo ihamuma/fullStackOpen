8b4494d76544040a8fd719c58d7de358c342a40f
ihamuma <you@example.com>
2020-10-14 12:47:10 +0300

    axios, minor changes to phonebook

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const SearchBox = ({ onChange, value }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Find countries: <input
+                        value={value}
+                        onChange={onChange}
+                      />
     </div>
-  );
+  )
+}
+
+const App = () => {
+  const [search, setSearch] = useState('')
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+        console.log(response.data)
+      })
+  }, [])
+
+  const searchChange = (event) => {
+    setSearch(event.target.value)
+  }
+
+  return (
+    <div>
+      <SearchBox value={search} onChange={searchChange} />
+    </div>
+  )
+
 }
 
 export default App;
