d3c69b32a278fc906a7686898142834420514125
ihamuma <you@example.com>
2020-10-14 13:36:02 +0300

    2.12

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -12,18 +12,51 @@ const SearchBox = ({ onChange, value }) => {
   )
 }
 
+const DisplayBox = ({ data, criteria }) => {
+
+  let filtered = data.filter(country => country.name.toLowerCase().includes(criteria.toLowerCase()))
+
+  if ( filtered.length > 10 || criteria === '' ) {
+    return (
+      <div>Too many matches, please specify another filter</div>
+    )
+  } if ( filtered.length === 1 ) {
+    return (
+      <div>
+        <h2>{filtered[0].name}</h2>
+        <p>
+          Capital: {filtered[0].capital} <br/>
+          Population: {filtered[0].population}
+        </p>
+        <h3>Languages</h3>
+        <div>
+          {filtered[0].languages.map(language => <p key={language.iso639_1} >• {language.name}</p>)}
+        </div>
+        <img width="300" src={filtered[0].flag} alt="" />
+      </div>
+    )
+  } if (filtered.length === 0) {
+    return (
+      <div>No matches, please specify another filter</div>
+    )
+  } else {
+    return (
+      <div>
+        {filtered.map(country => <p key={country.name}>{country.name}</p>)}
+      </div>
+    )
+  }
+}
+
 const App = () => {
   const [search, setSearch] = useState('')
   const [countries, setCountries] = useState([])
 
   useEffect(() => {
-    console.log('effect')
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        console.log('promise fulfilled')
         setCountries(response.data)
-        console.log(response.data)
       })
   }, [])
 
@@ -34,6 +67,7 @@ const App = () => {
   return (
     <div>
       <SearchBox value={search} onChange={searchChange} />
+      <DisplayBox data={countries} criteria={search} />
     </div>
   )
 
