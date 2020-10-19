2c12af551bf92b9ac0928eb53473252fca88f84d
ihamuma <you@example.com>
2020-10-19 21:11:31 +0300

    2.14

--- part2/countries/src/Components/Weather.js
+++ part2/countries/src/Components/Weather.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
+/* REACT_APP_API_KEY='767daf940b2468d69ef95639f5ef25f5' */
+
 const Weather = ({ location }) => {
+    const [ weatherData, setWeatherData ] = useState({location:{}, current: {}, request:{}})
+    
+    useEffect(() => {
+        axios.get(`http://api.weatherstack.com/current?access_key=${process.env.REACT_APP_API_KEY}&query=${location}`)
+          .then(response => {
+            setWeatherData(response.data)
+          })
+      }, [location])
+      console.log(process.env.REACT_APP_API_KEY)
+      console.log(weatherData)
+
     return (
-        <h3>Weather in {location}</h3>
+        <div>
+            <h3>Weather in {location}</h3>
+            <img src={weatherData.current.weather_icons} alt="" />
+            <div>Temperature: {weatherData.current.temperature} Celcius</div>
+            <div>Wind: {weatherData.current.wind_speed} kph, direction {weatherData.current.wind_dir}</div>
+        </div>
+
     )
 }
 
