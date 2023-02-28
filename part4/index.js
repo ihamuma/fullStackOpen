5b539c65db052e60fead2877a58ebc3e8a8b598b
ihamuma <markus.ihamuotila@gmail.com>
2023-02-28 11:06:09 +0200

    4.1. almost

--- part4/index.js
+++ part4/index.js
@@ -1,36 +1,7 @@
-// eslint-disable-next-line no-unused-vars
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const app = require('./app')
+const config = require('.utils/config')
+const logger = require('.utils/logger')
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
-})
-
-app.post('/api/blogs', (request, response) => {
-	const blog = new Blog(request.body)
-
-	blog
-		.save()
-		.then(result => {
-			response.status(201).json(result)
-		})
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+	logger.info(`Server running on port ${PORT}`)
 })
