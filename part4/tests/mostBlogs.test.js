88a042ee8b038d26ad027b26a5c95cbbe5941265
ihamuma <markus.ihamuotila@gmail.com>
2023-03-01 23:44:04 +0200

    4.6.

---
+++ part4/tests/mostBlogs.test.js
@@ -0,0 +1,29 @@
+const mostBlogs = require('../utils/list_helper').mostBlogs
+
+describe('most blogs', () => {
+    test('of an empty blog list is an empty author profile', () => {
+        const emptyList = []
+        expect(mostBlogs(emptyList)).toEqual({
+            author: null,
+            blogs: 0
+        })
+    })
+
+    test('of one blog is the author that blog', () => {
+        const listWithOneBlog = require('./testLists').listWithOneBlog
+        expect(mostBlogs(listWithOneBlog)).toEqual({
+            author: 'Edsger W. Dijkstra',
+            blogs: 1
+        })
+    })
+
+    test('of multiple blogs is the one with the most blogs', () => {
+        const blogsList = require('./testLists').blogsList
+        expect(mostBlogs(blogsList)).toEqual({
+            author: 'Robert C. Martin',
+            blogs: 3
+        })
+    })
+})
+
+module.exports = mostBlogs
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,9 +1,13 @@
+const lodash = require('lodash')
+
+// Dummy test function
 const dummy = (blogs) => {
     const len = (blogs.length)
     len
     return 1
 }
 
+// Return the total amount of likes in all blogs
 const totalLikes = (blogs) => {
     const likesReducer = (sum, item) => {
         return sum + item.likes
@@ -13,6 +17,7 @@ const totalLikes = (blogs) => {
         : blogs.reduce(likesReducer, 0)
 }
 
+// Return the title, author and likes of the blog with the most likes
 const favoriteBlog = (blogs) => {
     const formatBlog = (blog) => {
         delete blog['url']
@@ -36,8 +41,27 @@ const favoriteBlog = (blogs) => {
         : formatBlog(maxLikesBlog)
 }
 
+// Return the author with the most blogs and how many blogs they've written
+const mostBlogs = (blogs) => {
+    const authorBlogs = lodash.countBy(blogs, 'author')
+
+    const maxBlogsAuthor = lodash.maxBy(lodash.keys(authorBlogs), author => authorBlogs[author])
+    const maxBlogsCount = authorBlogs[maxBlogsAuthor]
+
+    return blogs.length === 0
+        ? {
+            author: null,
+            blogs: 0
+        }
+        : {
+            author: maxBlogsAuthor,
+            blogs: maxBlogsCount
+        }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
